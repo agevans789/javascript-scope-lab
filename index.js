8924eac5424070690c5ae4e7eaab183e295b4f72
Ann Grace Evans <agevans789@gmail.com>
2025-09-20 14:07:42 -0400

    step 3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your solution in this file!
+
+const burgers = ["Hamburger", "Cheeseburger"];
+const featuredDrink = "Strawberry Milkshake";
+
+function addBurger() {
+    var newBurger = "Flatburger";
+    burgers.push(newBurger); 
+    if () {
+        const anotherNewBurger = "Maple Bacon Burger"
+        burgers.push(anotherNewBurger)
+    }
+}
